fa392a397152a8c68c25c671682be63f697f54c7
itamar sandovski <itamarsando@gmail.com>
2019-01-20 17:28:47 +0200

    First commit

---
+++ src/views/layouts/FourLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-container grid-list-md text-xs-center fill-height="true">
+        <v-layout row wrap>
+            <v-flex v-for="i in 4" xs3>
+                <v-card dark color="primary" height="100%">
+                    <v-card-text class="px-0"></v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
